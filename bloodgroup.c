0ebff30c854bc819467e0ffb2efce576f9fc6a59
WarmEyes <gem.phantakarn@gmail.com>
2018-11-27 21:02:45 +0700

    edit Bloodgroup.c

--- bloodgroup.c
+++ bloodgroup.c
@@ -3,6 +3,8 @@
 void main()
 { char Name[40], G[5], answer[10];
   int i;
+  int error1bygem;
+  int error2bygem;
   do
   {
     printf("What is your Name? Jessica ? Leonardo ? Micahel Jackson ? ");
